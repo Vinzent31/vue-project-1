e5849384243f3cd7f10575129cdaeea1160bae02
Vinzent31 <vinzentgabriel60@gmail.com>
2026-09-08 20:07:45 +0700

    Membuat Landing Page

--- src/components/hero.vue
+++ src/components/hero.vue
@@ -7,7 +7,7 @@
         <!-- Background -->
         <div
             class="absolute inset-0 bg-cover bg-center"
-            < style="background-image: url('/src/assets/images/oke.png')" 
+            < style="background-image: url('/src/assets/images/hero.png')" 
         ></div>
 
         <!-- Overlay -->
